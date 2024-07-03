791f0981b7c5dbec317bef8cc80aedb1bcec5f44
Jonny Uribe <jouribe@starbucks.com>
2024-07-03 15:56:00 -0500

    init commit

---
+++ src/random_facts_wikipedia/console.py
@@ -0,0 +1,22 @@
+import textwrap
+
+import click
+import requests
+
+from . import __version__, wikipedia
+
+
+API_URL = "https://en.wikipedia.org/api/rest_v1/page/random/summary"
+
+
+@click.command()
+@click.version_option(version=__version__)
+def main():
+    """The hypermodern Python project."""
+    data = wikipedia.random_page()
+
+    title = data["title"]
+    extract = data["extract"]
+
+    click.secho(title, fg="green")
+    click.echo(textwrap.fill(extract))
