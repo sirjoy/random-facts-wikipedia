0d97fd967cce1c67e2ef91025b180d1d1b404293
Jonny Uribe <jouribe@starbucks.com>
2024-07-05 15:53:10 -0500

    add unit tests

--- src/random_facts_wikipedia/console.py
+++ src/random_facts_wikipedia/console.py
@@ -5,15 +5,19 @@ import requests
 
 from . import __version__, wikipedia
 
-
-API_URL = "https://en.wikipedia.org/api/rest_v1/page/random/summary"
-
-
 @click.command()
+@click.option(
+    "--language",
+    "-l",
+    default="en",
+    help="Language edition of Wikipedia",
+    metavar="LANG",
+    show_default=True,
+)
 @click.version_option(version=__version__)
-def main():
+def main(language):
     """The hypermodern Python project."""
-    data = wikipedia.random_page()
+    data = wikipedia.random_page(language=language)
 
     title = data["title"]
     extract = data["extract"]
